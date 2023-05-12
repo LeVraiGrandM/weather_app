bab58662c88638ea7582b24beeca7454b5796975
maxence2008 <88449183+maxence2008@users.noreply.github.com>
2023-05-12 19:15:30 +0200

    feat: finalization of the json file and implementation of the illustration image and description connected to the api

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 //fetch pour météo de paris => tableau
 //fonction render avec comme paramètre le tableau
 //possibilité de chaanger la localisation avec soit : recherche ou géolocalisation
-import test from "weather_codes.js";
 
 let latitude = 48.85;
 let longitude = 2.34;
 let unit = "°C";
+let weather_codes;
+
+fetch("./weather_codes.json")
+   .then((res) => res.json())
+   .then((data) => (weather_codes = data));
 
 let data = fetch(
-   `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+   `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=Europe/Paris&current_weather=true&daily=weathercode`
 )
    .then((res) => res.json())
    .then((data) => render(data));
@@ -18,11 +22,16 @@ function render(data) {
    const temp = document.querySelector(".temp");
    const day = document.querySelector(".day");
    const hour = document.querySelector(".hour");
+   const weather_icon = document.querySelector(".weather_icon");
+   const description = document.querySelector(".description");
    let date = convertTZ("Europe/Paris");
 
    temp.innerHTML = Math.round(data.current_weather.temperature) + unit;
    day.innerHTML = getDayName("en-fr") + ",";
    hour.innerHTML = date.substring(0, 5);
+   weather_icon.src = weather_codes[data.daily.weathercode[0]].image;
+   description.innerHTML +=
+      weather_codes[data.daily.weathercode[0]].description;
 }
 
 function convertTZ(tzString) {
@@ -33,5 +42,3 @@ function getDayName(locale) {
    let date = new Date();
    return date.toLocaleDateString(locale, { weekday: "long" });
 }
-
-console.log(test);
