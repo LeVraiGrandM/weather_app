58e32414c49255b2fbc64db1be6f54bb34b63e88
maxence2008 <88449183+maxence2008@users.noreply.github.com>
2023-05-17 19:06:33 +0200

    fix: data display problems

--- script.js
+++ script.js
@@ -8,25 +8,24 @@ let latitude = 48.85;
 let longitude = 2.34;
 let city = "Paris";
 let unit = "°C";
-let weather_codes;
-
 //comportement
 
-fetch("./weather_codes.json")
-   .then((res) => res.json())
-   .then((data) => (weather_codes = data))
-   .catch((e) => {
-      new Error(e);
-   });
+async function fetchData() {
+   let weatherCodes = await fetch("./weather_codes.json")
+      .then((res) => res.json())
+      .catch((e) => {
+         new Error(e);
+      });
 
-fetch(
-   `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=auto&current_weather=true&daily=precipitation_probability_mean,weathercode,temperature_2m_max,temperature_2m_min`
-)
-   .then((res) => res.json())
-   .then((data) => {
-      renderAside(data);
-      renderWeekOverview(data);
-   });
+   let data = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=auto&current_weather=true&daily=precipitation_probability_mean,weathercode,temperature_2m_max,temperature_2m_min`
+   ).then((res) => res.json());
+
+   renderAside(data, weatherCodes);
+   renderWeekOverview(data, weatherCodes);
+}
+
+fetchData();
 
 function convertTZ(tzString) {
    return new Date().toLocaleTimeString("fr", { timeZone: tzString });
@@ -36,36 +35,28 @@ function getDayName(locale) {
    let date = new Date();
    return date.toLocaleDateString(locale, { weekday: "long" });
 }
-
-//.forEach((elem) => {
-//   elem.addEventListener("click", (e) => {
-//       console.log(e);
-//    });
-// });
-
 //render
 
-function renderAside(data) {
+async function renderAside(data, weatherCodes) {
    const temp = document.querySelector(".temp");
    const day = document.querySelector(".day");
    const hour = document.querySelector(".hour");
    const weather_icon = document.querySelector(".weather-illustration");
    const description = document.querySelector(".description");
    const rain = document.querySelector(".rain");
-
    let date = convertTZ(data.timezone);
 
    temp.innerHTML = Math.round(data.current_weather.temperature) + unit;
    day.innerHTML = getDayName("en-fr") + ",";
    hour.innerHTML = date.substring(0, 5);
    weather_icon.style.backgroundPosition =
-      weather_codes[data.current_weather.weathercode].image;
+      weatherCodes[data.current_weather.weathercode].image;
    description.innerHTML +=
-      weather_codes[data.current_weather.weathercode].description;
+      weatherCodes[data.current_weather.weathercode].description;
    rain.innerHTML += `Rain - ${data.daily.precipitation_probability_mean[0]}%`;
 }
 
-function renderWeekOverview(data) {
+async function renderWeekOverview(data, weatherCodes) {
    const weekOverview = document.querySelector(".week-overview");
 
    for (let i = 0; i < 7; i++) {
@@ -74,7 +65,7 @@ function renderWeekOverview(data) {
             weekday: "long",
          })}</p>
          <div class="daily-image-container" style="background-position: ${
-            weather_codes[data.daily.weathercode[i]].weekOverview
+            weatherCodes[data.daily.weathercode[i]].weekOverview
          }"></div>
          <div class="daytime-temperature">
            <span class="max">${Math.round(
